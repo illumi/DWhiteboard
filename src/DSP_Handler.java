24afcf56004c6adf4adb562f57daef865824d4ec
Jakub Chlanda <j.chlanda@gmail.com>
2011-11-26 23:20:42 +0000

    simple socet server and client, run java DSP_Server to set up the server. and than telnet to it, server listens on port: 11111

---
+++ src/DSP_Handler.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.net.*;
+
+public class DSP_Handler extends Thread {
+	final private Socket client;
+	final private DSP_Server server;
+	final private BufferedReader input;
+	final private PrintWriter output; 
+
+	public DSP_Handler(DSP_Server server, Socket client) throws IOException {
+		this.server = server;
+		this.client = client;
+
+		input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+		output = new PrintWriter(client.getOutputStream(), true);
+	}
+
+	public void run() {
+		try {
+
+			output.println("In DSP_Handler.run()");
+
+			String message = input.readLine(); 
+
+			DSP_User u = server.addUser(this, message);
+
+			server.sendChat(this, "user id: " + u.id + " user name: " + u.name);
+
+			// Begin handling loop
+
+			while ((message = readMessage()) != null) {
+				handleMessage(message);
+			}	
+
+		}
+		catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		finally {
+			try {
+				server.removeUser(this);
+
+				output.close();
+				input.close();
+				client.close();
+			}
+			catch (IOException e) {
+				System.err.println(e.getMessage());
+			}
+		}
+	}
+
+	//get input from terminal
+	public String readMessage() {
+		try{
+			String line = input.readLine().trim();
+			if(line.equals(""))
+				return null;
+			return line;
+		}
+		catch (IOException e){
+			System.err.println("IO Error"); 
+			return null;
+		}
+	}
+
+	private void handleMessage(String message) throws Exception {
+		if(message.startsWith("chat:"))
+			server.sendChat(this, message);
+		else if(message.startsWith("exit:"))
+			server.removeUser(this);
+		else
+			server.sendChat(this, "Unknow message: " + message);
+	}
+
+	public void sendMessage(String message) {
+		output.println(message);
+	}
+
+}
